f19c47d919959cd10b93e5570577bd392c6e9fe1
Manuel Avila Rodriguez <mastercraft9000@gmail.com>
2021-01-02 00:26:03 +0000

    Funcionamiento TEP

--- app/src/main/java/itcm/CataogActivity.java
+++ app/src/main/java/itcm/CataogActivity.java
@@ -1,3 +1,5 @@
+package itcm;
+
 import java.util.List;
 
 import android.app.Activity;
@@ -12,42 +14,41 @@ import android.widget.AdapterView.OnItemClickListener;
 
 public class CatalogActivity extends Activity {
 
- private List<Product> mProductList;
+    private List<Product> mProductList;
 
- /** 
-  * Called when the activity is first created. 
-  */
- @Override
- public void onCreate(Bundle savedInstanceState) {
-  super.onCreate(savedInstanceState);
-  setContentView(R.layout.catalog);
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.catalog);
 
-  // Obtain a reference to the product catalog
-  mProductList = ShoppingCartHelper.getCatalog(getResources());
-  
-  // Create the list
-  ListView listViewCatalog = (ListView) findViewById(R.id.ListViewCatalog);
-  listViewCatalog.setAdapter(new ProductAdapter(mProductList, getLayoutInflater(), false));
-  
-  listViewCatalog.setOnItemClickListener(new OnItemClickListener() {
+        // Obtain a reference to the product catalog
+        mProductList = ShoppingCartHelper.getCatalog(getResources());
 
-   @Override
-   public void onItemClick(AdapterView<?> parent, View view, int position,
-     long id) {
-    Intent productDetailsIntent = new Intent(getBaseContext(),ProductDetailsActivity.class);
-    productDetailsIntent.putExtra(ShoppingCartHelper.PRODUCT_INDEX, position);
-    startActivity(productDetailsIntent);
-   }
-  });
-  
-  Button viewShoppingCart = (Button) findViewById(R.id.ButtonViewCart);
-  viewShoppingCart.setOnClickListener(new OnClickListener() {
-   
-   @Override
-   public void onClick(View v) {
-    Intent viewShoppingCartIntent = new Intent(getBaseContext(), ShoppingCartActivity.class);
-    startActivity(viewShoppingCartIntent);
-   }
-  });
- }
+        // Create the list
+        ListView listViewCatalog = (ListView) findViewById(R.id.ListViewCatalog);
+        listViewCatalog.setAdapter(new ProductAdapter(mProductList, getLayoutInflater(), false));
+
+        listViewCatalog.setOnItemClickListener(new OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent productDetailsIntent = new Intent(getBaseContext(), ProductDetailsActivity.class);
+                productDetailsIntent.putExtra(ShoppingCartHelper.PRODUCT_INDEX, position);
+                startActivity(productDetailsIntent);
+            }
+        });
+
+        Button viewShoppingCart = (Button) findViewById(R.id.ButtonViewCart);
+        viewShoppingCart.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent viewShoppingCartIntent = new Intent(getBaseContext(), ShoppingCartActivity.class);
+                startActivity(viewShoppingCartIntent);
+            }
+        });
+    }
 }
