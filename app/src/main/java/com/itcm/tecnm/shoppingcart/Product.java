15ec902e982526c02c8b3a93ca74fd0a247c5306
Manuel Avila Rodriguez <mastercraft9000@gmail.com>
2021-01-02 01:02:24 +0000

    Modificado y actualizado

---
+++ app/src/main/java/com/itcm/tecnm/shoppingcart/Product.java
@@ -0,0 +1,18 @@
+package com.itcm.tecnm.shoppingcart;
+
+import android.graphics.drawable.Drawable;
+
+public class Product {
+
+  public String title;
+  public String description;
+  public double price;
+  public boolean selected;
+
+  public Product(String title, String description, double price) {
+    this.title = title;
+    this.description = description;
+    this.price = price;
+  }
+
+}
